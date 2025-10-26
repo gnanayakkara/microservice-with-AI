8124bf98a963cd2c6ba67340b377e851530e74a4
Gayan Nanayakkara <34508294+gnanayakkara@users.noreply.github.com>
2025-10-26 16:13:52 +0400

    Initial changes for Activity Service

---
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -0,0 +1,35 @@
+package com.fitness.activityservice.controller;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.service.ActivityService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/activities")
+@AllArgsConstructor
+public class ActivityController {
+
+    private ActivityService activityService;
+    @PostMapping
+    public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest activityRequest){
+
+        return ResponseEntity.ok(activityService.trackActivity(activityRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ActivityResponse>> getUserActivites(@RequestHeader("X-User-ID") String userId){
+
+        return ResponseEntity.ok(activityService.getUserActivites(userId));
+    }
+
+    @GetMapping("/{activityId}")
+    public ResponseEntity<ActivityResponse> getActivity(@PathVariable("activityId") String activityId){
+
+        return ResponseEntity.ok(activityService.getActivityById(activityId));
+    }
+}
